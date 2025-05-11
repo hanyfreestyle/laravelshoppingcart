05e087572ec17cdd6997bb458dd0d9a95edf00a5
Hany Darwish <hany.freestyle4u@gmail.com>
2025-05-11 14:37:56 +0300

    Update composer.json to support Laravel 11

--- src/ShoppingcartServiceProvider.php
+++ src/ShoppingcartServiceProvider.php
@@ -8,33 +8,35 @@ use Illuminate\Support\ServiceProvider;
 
 class ShoppingcartServiceProvider extends ServiceProvider
 {
-
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
+    public function register(): void
     {
-        $this->app->bind('cart', 'FreestyleRepo\Shoppingcart\Cart');
+        // Binding the cart service
+        $this->app->bind('cart', Cart::class);
 
-        $config = __DIR__ . '/../config/cart.php';
-        $this->mergeConfigFrom($config, 'cart');
+        // Merge the config
+        $this->mergeConfigFrom(__DIR__ . '/../config/cart.php', 'cart');
+    }
 
-        $this->publishes([__DIR__ . '/../config/cart.php' => config_path('cart.php')], 'config');
+    public function boot(): void
+    {
+        // Publish the config
+        $this->publishes([
+            __DIR__ . '/../config/cart.php' => config_path('cart.php')
+        ], 'config');
 
+        // Destroy cart on logout
         $this->app['events']->listen(Logout::class, function () {
-            if ($this->app['config']->get('cart.destroy_on_logout')) {
-                $this->app->make(SessionManager::class)->forget('cart');
+            if (config('cart.destroy_on_logout')) {
+                app(SessionManager::class)->forget('cart');
             }
         });
 
-        if ( ! class_exists('CreateShoppingcartTable')) {
-            // Publish the migration
-            $timestamp = date('Y_m_d_His', time());
-
+        // Publish migration if class doesn't exist
+        if (!class_exists('CreateShoppingcartTable')) {
+            $timestamp = date('Y_m_d_His');
             $this->publishes([
-                __DIR__ . '/../database/migrations/0000_00_00_000000_create_shopping_cart_table.php' => database_path('migrations/'.$timestamp.'_create_shoppingcart_table.php'),
+                __DIR__ . '/../database/migrations/0000_00_00_000000_create_shopping_cart_table.php'
+                => database_path('migrations/' . $timestamp . '_create_shoppingcart_table.php'),
             ], 'migrations');
         }
     }
